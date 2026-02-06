71e0d93dd34d1e5da9cca562ce1e6b50e5df85d7
Uhirwa-melissa54/ <uhirwamelissa@gmail.com>
2026-02-06 20:06:19 +0200

    did the studentEntity

---
+++ src/main/java/com/relean/lean/entities/Student.java
@@ -0,0 +1,37 @@
+package com.relean.lean.entities;
+
+//import com.relearn.auth.Role;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "students")
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+@Builder
+public class Student {
+
+    @Id
+    @Column(nullable = false, unique = true)
+    private Long studentId;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String gender;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+}
+
