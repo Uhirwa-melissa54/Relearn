235a756484379669e3f9ed7a84ca4838dbebc6b1
Uhirwa-melissa54/ <uhirwamelissa@gmail.com>
2026-02-07 18:08:45 +0200

    added teacher entity

--- src/main/java/com/relean/lean/entities/Teacher.java
+++ src/main/java/com/relean/lean/entities/Teacher.java
@@ -1,4 +1,38 @@
 package com.relean.lean.entities;
 
+
+
+import com.relean.lean.roles.RoleEnum;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "teachers")
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+@Builder
 public class Teacher {
+
+    @Id
+    private Long teacherId;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private String gender;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String passwordHash;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private RoleEnum role;
 }
+
