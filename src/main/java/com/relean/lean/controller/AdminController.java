059cd993f414d1d85797145393b177b798aef969
Uhirwa-melissa54/ <uhirwamelissa@gmail.com>
2026-02-08 21:14:17 +0200

    added adminController to handle thee adminService

--- src/main/java/com/relean/lean/controller/AdminController.java
+++ src/main/java/com/relean/lean/controller/AdminController.java
@@ -1,4 +1,37 @@
 package com.relean.lean.controller;
 
+import com.relean.lean.dtos.RegisterResponse;
+import com.relean.lean.dtos.StudentRegisterRequestDto;
+import com.relean.lean.dtos.TeacherRequestDto;
+import com.relean.lean.service.AdminService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/admin")
+@RequiredArgsConstructor
 public class AdminController {
+
+    private final AdminService adminService;
+
+
+
+    @PostMapping("/register-student")
+    public ResponseEntity<RegisterResponse> registerStudent(
+            @RequestBody StudentRegisterRequestDto req
+    ) {
+        RegisterResponse response = adminService.registerStudent(req);
+        return ResponseEntity.ok(response);
+    }
+
+
+    @PostMapping("/register-teacher")
+    public ResponseEntity<RegisterResponse> registerTeacher(
+            @RequestBody TeacherRequestDto req
+    ) {
+        RegisterResponse response = adminService.registerTeacher(req);
+        return ResponseEntity.ok(response);
+    }
 }
+
