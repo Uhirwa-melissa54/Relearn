08e7f1380d857f1e9d15c1cace3fe9b2a77310a8
Uhirwa-melissa54/ <uhirwamelissa@gmail.com>
2026-02-12 17:49:51 +0200

    added emailsDetails

--- src/main/java/com/relean/lean/dtos/EmailDto.java
+++ src/main/java/com/relean/lean/dtos/EmailDto.java
@@ -1,4 +1,13 @@
 package com.relean.lean.dtos;
 
+import lombok.Data;
+
+@Data
 public class EmailDto {
+    private String fullName;
+    private String email;
+    private String password;
+
+
+
 }
