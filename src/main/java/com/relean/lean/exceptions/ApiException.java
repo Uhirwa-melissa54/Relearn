e2ced9c9376841ea83e7acef6d684ac9ac9d4ae4
Uhirwa-melissa54/ <uhirwamelissa@gmail.com>
2026-02-26 10:14:13 +0200

    handling Exceptions

---
+++ src/main/java/com/relean/lean/exceptions/ApiException.java
@@ -0,0 +1,17 @@
+package com.relean.lean.exceptions;
+
+import org.springframework.http.HttpStatus;
+
+public class ApiException extends RuntimeException {
+
+    private HttpStatus status;
+
+    public ApiException(HttpStatus status, String message) {
+        super(message);
+        this.status = status;
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+}
