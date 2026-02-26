78a90124e2a324d84344a923397314412946f983
Uhirwa-melissa54/ <uhirwamelissa@gmail.com>
2026-02-26 09:44:23 +0200

    added studentService

--- src/main/java/com/relean/lean/dtos/LoginResponse.java
+++ src/main/java/com/relean/lean/dtos/LoginResponse.java
@@ -1,5 +1,8 @@
 package com.relean.lean.dtos;
 
+import lombok.Builder;
+
+@Builder
 public class LoginResponse {
     String email;
     String token;
--- src/main/java/com/relean/lean/service/StudentService.java
+++ src/main/java/com/relean/lean/service/StudentService.java
@@ -1,4 +1,42 @@
 package com.relean.lean.service;
 
-public class StudentService {
+import com.relean.lean.dtos.LoginRequest;
+import com.relean.lean.dtos.LoginResponse;
+import com.relean.lean.entities.Student;
+import com.relean.lean.repository.StudentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final StudentRepository studentRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+
+    public LoginResponse login(LoginRequest request) {
+
+        Student student = studentRepository.findByEmail(request.getEmail())
+                .orElseThrow(() -> new RuntimeException("Invalid email or password"));
+
+        if (!passwordEncoder.matches(request.getPassword(), student.getPassword())) {
+            throw new RuntimeException("Invalid email or password");
+        }
+
+        String accessToken = jwtService.generateAccessToken(
+                student.getEmail(),
+                student.getRole()
+        );
+
+        String refreshToken = jwtService.generateRefreshToken(
+                student.getEmail()
+        );
+
+        return LoginResponse.builder()
+                .accessToken(accessToken)
+                .refreshToken(refreshToken)
+                .build();
+    }
 }
